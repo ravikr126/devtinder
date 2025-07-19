bad2d9642d5a3e1339810226bc5ef5bd5b30fc49
ravikr126 <ravi14577@gmail.com>
2025-07-20 01:15:09 +0530

    s2 ep3

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log('hi')
+const express= require('express')
+const app= express()
+
+// app.use((req,res)=>{
+//     res.send("Hello from the server")
+// })
+
+app.use("/hello",(req,res)=>{
+    res.send("Hello ")
+})
+
+app.use("/master",(req,res)=>{
+    res.send("Hello master")
+})
+
+app.listen(3000,()=>{
+    console.log("sercer is sucessfully run on port 3000")
+})
